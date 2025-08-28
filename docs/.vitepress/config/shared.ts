0ca23da8d8832887fcf0a618b0ea47610a8e78b3
mateogic <ljc_2318516761@163.com>
2025-08-29 00:44:05 +0800

    test

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -5,7 +5,6 @@ import Components from 'unplugin-vue-components/vite';
 import { TDesignResolver } from 'unplugin-vue-components/resolvers';
 import viteCompression from 'vite-plugin-compression';
 
-import { createRssFileZH, createRssFileEN } from "../theme/utils/rss";
 import { handleHeadMeta } from "../theme/utils/handleHeadMeta";
 import { search as zhSearch } from './zh'
 
@@ -40,33 +39,29 @@ export default defineConfig({
   async transformHead(context) {
     return handleHeadMeta(context)
   },
-  buildEnd: (config: SiteConfig) => {
-    createRssFileZH(config);
-    createRssFileEN(config);
-  },
 
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     outline: [2, 4],
 
     // 本地搜索
-    // search: {
-    //   provider: "local",
-    //   options: {
-    //     locales: { ...zhSearch }
-    //   }
-    // },
-
-    // algolia搜索，需要申请，如不需要，删除下面的配置，可使用本地搜索
     search: {
-      provider: 'algolia',
+      provider: "local",
       options: {
-        appId: 'LGWG5THRKY',
-        apiKey: '8fb5c1dc72bc92580f7fa1471ad2b814',
-        indexName: 'Mateogic',
         locales: { ...zhSearch }
       }
     },
+
+    // // algolia搜索，需要申请，如不需要，删除下面的配置，可使用本地搜索
+    // search: {
+    //   provider: 'algolia',
+    //   options: {
+    //     appId: 'LGWG5THRKY',
+    //     apiKey: '8fb5c1dc72bc92580f7fa1471ad2b814',
+    //     indexName: 'Mateogic',
+    //     locales: { ...zhSearch }
+    //   }
+    // },
 
     externalLinkIcon: true,
   },
